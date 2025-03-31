e582570f6a69f7f8a902687d05c82c7584b4783b
Selena <brutix80@gmail.com>
2025-03-31 03:45:15 +0300

    last comm

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const navLinks = document.querySelector('.nav-links');
 burger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+
+document.querySelectorAll('.zoomable').forEach(img => {
+    img.addEventListener('click', function () {
+      const overlay = document.createElement('div');
+      overlay.classList.add('overlay');
+      document.body.appendChild(overlay);
+      overlay.style.display = 'block';
+  
+      this.classList.add('zoomed');
+  
+      overlay.addEventListener('click', () => {
+        this.classList.remove('zoomed');
+        overlay.remove();
+      });
+    });
+  });
